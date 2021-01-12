976413fe96b6134e06dbdd4ca936367d2576fa30
Rafin Reza <rafintokyo@gmail.com>
2021-01-12 22:28:55 +0000

    added edge cases

--- timer1.js
+++ timer1.js
@@ -1,12 +1,13 @@
 const input = process.argv.splice(2).sort((a,b) => (a-b));;
 const nums = input.map(Number);
-console.log(nums);
 const beeper = () => {
   for (let i = 0; i < nums.length; i++){
-    let timer = ((nums[i])*1000);
-    setTimeout(() => {
-    process.stdout.write(`*BEEP!*\n`);
-   }, timer);
+    if (nums[i] > 0 && !isNaN(nums[i])) {
+      let timer = ((nums[i])*1000)
+      setTimeout(() => {
+      process.stdout.write(`*BEEP!*\n`);
+      }, timer);
+    }
   }
 }
 beeper();
